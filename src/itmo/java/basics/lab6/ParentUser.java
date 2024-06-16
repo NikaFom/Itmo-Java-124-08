12f8b61cb9b6e3374f048f455722b8d84f7d0ac9
Veronika Rudenko <veronika.fomishina@gmail.com>
2024-06-16 17:42:27 +0500

    Lab6

--- src/itmo/java/basics/lab6/ParentUser.java
+++ src/itmo/java/basics/lab6/ParentUser.java
@@ -1,9 +1,9 @@
 package itmo.java.basics.lab6;
 import java.util.Scanner;
 public class ParentUser {
-    Scanner scanner = new Scanner(System.in);
-    int age = scanner.nextInt();
-    String name = scanner.next();
+    protected Scanner scanner = new Scanner(System.in);
+    protected int age = scanner.nextInt();
+    protected String name = scanner.next();
 
     public void userInfo() {
         System.out.println("Возраст пользователя: " + age);
