32e32f0b85457b7803c32cecf6e613431762d5db
Veronika Rudenko <veronika.fomishina@gmail.com>
2024-06-18 16:36:19 +0500

    lab7

--- src/itmo/java/basics/lab7/Main.java
+++ src/itmo/java/basics/lab7/Main.java
@@ -2,7 +2,13 @@ package itmo.java.basics.lab7;
 
 public class Main {
     public static void main(String[] args) {
-        Plane plane = new Plane();
-        plane.innerMethodCall();
+        Plane.Wing wing1 = new Plane.Wing();
+        wing1.printWingWeight(500);
+
+        Plane.Wing wing2 = new Plane.Wing();
+        wing2.printWingWeight(1000);
+
+        Plane.Wing wing3 = new Plane.Wing();
+        wing3.printWingWeight(2000);
     }
 }
--- src/itmo/java/basics/lab7/Plane.java
+++ src/itmo/java/basics/lab7/Plane.java
@@ -1,25 +1,11 @@
 package itmo.java.basics.lab7;
 
 public class Plane {
-    class Wing {
+    static class Wing {
         private Integer wingWeight;
 
-        void printWingWeight() {
+        void printWingWeight(Integer wingWeight) {
             System.out.println("Вес крыла самолета - " + wingWeight);
         }
-    }
-
-    public void innerMethodCall() {
-        Wing wing1 = new Wing();
-        wing1.wingWeight = 500;
-        wing1.printWingWeight();
-
-        Wing wing2 = new Wing();
-        wing2.wingWeight = 1000;
-        wing2.printWingWeight();
-
-        Wing wing3 = new Wing();
-        wing3.wingWeight = 2000;
-        wing3.printWingWeight();
     }
 }
